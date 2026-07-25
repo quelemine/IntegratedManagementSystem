c9d3b7120c13847752ce91c6151438803f9d4f4a
quelemine <iquelemine@byupathway.edu>
2026-07-25 10:17:48 +0200

    Fix alter_payments migration - remove non-existent column
    - Remove received_by column alteration (column doesn't exist in payments)
    - Payments table has recorded_by instead of received_by
    - This fixes the migration failure

--- server/migrations/20240116000052_alter_payments_add_invoice_id.js
+++ server/migrations/20240116000052_alter_payments_add_invoice_id.js
@@ -5,7 +5,6 @@
 exports.up = function(knex) {
   return knex.schema.alterTable('payments', (table) => {
     table.uuid('invoice_id').references('id').inTable('invoices').nullable();
-    table.uuid('received_by').references('id').inTable('users').nullable().alter();
   });
 };
 
